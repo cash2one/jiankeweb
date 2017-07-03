c8ad13d56fff346636d1c279020f82a590313012
liling <liling@jk.com>
2017-07-03 21:19:00 +0800

    modify chart

--- data_web/mydata/plots.py
+++ data_web/mydata/plots.py
@@ -24,7 +24,7 @@ class SeriesCharts(object):
         '''
         折线图
         '''
-        sql = 'select collect_time, sum(old_sale_num_30) sum_old, sum(sale_num_growth) sum_new  from shw_tmall_baojian_week group by collect_time;'
+        sql = 'select collect_time, sum(old_sale_num_30) sum_old, sum(IFNULL(old_sale_num_30,0) + IFNULL(sale_num_growth,0)) sum_new  from shw_tmall_baojian_week group by collect_time;'
         df = pd.read_sql(sql, self.db_conn)
         trace_old = go.Scatter(
                 x=df['collect_time'],
@@ -36,14 +36,23 @@ class SeriesCharts(object):
                 x=df['collect_time'],
                 y=df['sum_new'],
                 name='本月',
-                line = dict(color = '#7F7F7F'),
+                line = dict(color = '#FF77FF'),
                 opacity = 0.8)
 
         data = [trace_old, trace_new]
         layout = dict(
-                title = "Manually Set Date Range",
+                height = 280,
+                #title = "Manually Set Date Range",
+                yaxis = dict(
+                    range = [0,300000]),
                 xaxis = dict(
-                    range = ['2017-07-03','2016-11-21'])
+                    range = ['2017-07-03','2016-11-21']),
+                margin = dict(
+                    l=65,
+                    r=50,
+                    b=55,
+                    t=30
+                    )
                 )
         fig = dict(data=data, layout=layout)
         #import pdb
